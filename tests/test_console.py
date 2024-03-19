4811d56dbb094e2f4369d2abdcc9ad5c8445ba4a
ayoubleass <ayoubleas@gmail.com>
2024-03-19 06:59:01 -0700

    simple bash for help bashScript

---
+++ tests/test_console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+A unit test module for the console (command interpreter).
+"""
+import json
+import MySQLdb
+import os
+import sqlalchemy
+import unittest
+from io import StringIO
+from unittest.mock import patch
+
+from console import HBNBCommand
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+from tests import clear_stream
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """
+    """
+
+
+
+    def test_return_value(self):
+        #conosle = HBNBCommand()
+        #console.onecmd('create City name="Califronia"')
+        pass
+
