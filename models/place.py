74743573ac8f80be90a1e52ecc949a7bd67b0882
MohamedAlaga <malaga.142014@gmail.com>
2024-03-20 05:49:15 +0200

    task 10 update

--- models/place.py
+++ models/place.py
@@ -51,7 +51,7 @@ class Place(BaseModel, Base):
             from models.amenity import Amenity
             if isinstance(obj, Amenity):
                 self.amenity_ids.append(obj.id)
-if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenity = Table('place_amenity', Base.metadata,
+                
+place_amenity = Table('place_amenity', Base.metadata,
                         Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
                         Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True, nullable=False))
