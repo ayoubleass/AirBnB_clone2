8bd2d510f2189f7cca4e2bfdc5ba1cfd9dd431c1
MohamedAlaga <malaga.142014@gmail.com>
2024-03-20 06:30:25 +0200

    handel if = not in arguments

--- console.py
+++ console.py
@@ -126,9 +126,8 @@ class HBNBCommand(cmd.Cmd):
         new_args = args.split()[1:]
         obj_dict = {}
         for arg  in new_args:
-            if "=" not in arg:
-                continue
-            key, value = arg.split("=")
+            if "=" in arg:
+                key, value = arg.split("=")
             if value.startswith('"') and value.endswith('"'):
                 value = value.strip('"')
                 if "_" in value:
