f7fd320e331261f8bc3c0f96147c98203446fa51
MohamedAlaga <malaga.142014@gmail.com>
2024-03-20 02:46:39 +0200

    solve styling in user class

--- models/user.py
+++ models/user.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from models.base_model import Base 
+from models.base_model import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-class User(BaseModel,Base):
+
+
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128),nullable=False)
-    password = Column(String(128),nullable=False)
-    first_name = Column(String(128),nullable=True)
-    last_name = Column(String(128),nullable=True)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
