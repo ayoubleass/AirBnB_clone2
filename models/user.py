435cab81e88cbf5af0cfe647f1aeb1e91fd88868
MohamedAlaga <malaga.142014@gmail.com>
2024-03-20 03:16:22 +0200

    remoce task 8 changes

--- models/user.py
+++ models/user.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,4 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
-
