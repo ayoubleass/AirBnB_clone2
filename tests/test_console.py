f40f137e2bec8f6e0f9bab92535af3c2a630f489
MohamedAlaga <malaga.142014@gmail.com>
2024-03-21 03:53:31 +0200

    add some test cases for the console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -20,8 +20,29 @@ from tests import clear_stream
 class TestHBNBCommand(unittest.TestCase):
     """
     """
+    def test_all(self):
+        """ test all method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("all Houses")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
+    def test_create(self):
+        """ test create method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("create Houses")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
+    def test_update(self):
+        """ test update method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("update Houses")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
+    
+    def test_destroy(self):
+        """ test destroy method """
+        with patch('sys.stdout', new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy Houses")
+            self.assertEqual(f.getvalue(), "\n** class doesn't exist **\n")
 
     def test_return_value(self):
         #conosle = HBNBCommand()
