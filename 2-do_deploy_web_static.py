4656196a69ffe42f6724f8701ec0c0de09b93f42
ayoubleass <ayoubleas@gmail.com>
2024-04-05 09:38:40 -0700

    fixing task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,18 +12,22 @@ def do_deploy(archive_path):
     """
     Distributes an archive to your web servers
     """
-    if  exists(archive_path) is False:
-        return False;
+    if exists(archive_path) is False:
+        return False
     filename = archive_path.split('/')[-1]
-    filenamePathwithNoExtension = '/data/web_static/releases/{}'.format(filename.split(".")[0])
+    filenamePathwithNoExtension = '/data/web_static/releases/{}'.format(
+            filename.split(".")[0]
+            )
     temp = "/tmp/{}".format(filename)
     try:
         put(archive_path, "/tmp")
         run("mkdir -p ")
-        run("tar -xzf {} -C {}/".format(temp , filenamePathwithNoExtension))
+        run("tar -xzf {} -C {}/".format(temp, filenamePathwithNoExtension))
         run("rm {}".format(temp))
         run("rm -r /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(filenamePathwithNoExtension))
+        run("ln -s {}/ /data/web_static/current".format(
+            filenamePathwithNoExtension)
+            )
         return True
     except Exception:
         return False
