e607214bd664721a63d3a1ca05b2157cc2c78cc4
ayoubleass <ayoubleas@gmail.com>
2024-04-05 11:48:28 -0700

    fixing task2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,15 +7,17 @@ This module contains the do_deploy
 from fabric.api import *
 from os.path import exists
 
+
 env.hosts = ["100.26.168.20", "100.25.141.186"]
+
 
 def do_deploy(archive_path):
     """
     Distributes an archive to your web servers
     """
-    if exists(archive_path) is False:
+    if not exists(archive_path):
         return False
-    # -> path version/ 
+    # web_static_20170315003959.tgz
     filename = archive_path.split('/')[-1]
     filenamePathwithNoExtension = '/data/web_static/releases/{}'.format(
             filename.split(".")[0]
@@ -23,15 +25,19 @@ def do_deploy(archive_path):
     temp = "/tmp/{}".format(filename)
     try:
         put(archive_path, "/tmp")
-        run("mkdir -p {}".format(filenamePathwithNoExtension))
-        print(filenamePathwithNoExtension)
-        run("tar -xzf {} -C {}/".format(temp, filenamePathwithNoExtension))
-        run("rm {}".format(temp))
-        run("rm -r /data/web_static/current")
-        run("ln -s {}/ /data/web_static/current".format(
+        run("sudo mkdir -p {}".format(filenamePathwithNoExtension))
+        run("sudo tar -xvzf {} -C {}/".format(
+            temp, filenamePathwithNoExtension
+            ))
+        run("sudo mv {}/web_static/* {}".format(
+            filenamePathwithNoExtension, filenamePathwithNoExtension)
+            )
+        run("sudo rm {}".format(temp))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {}/ /data/web_static/current".format(
             filenamePathwithNoExtension)
             )
         return True
-    except Exception:
-        print("CComand failed")
+    except Exception as e:
+        print("{}".format(e))
         return False
