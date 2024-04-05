d54c0ca08e87801b9073aeea8cc214050339ff96
ayoubleass <ayoubleas@gmail.com>
2024-04-05 09:33:58 -0700

    fixing task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 """
-This module has the function do pach which generates a .tgz archive 
+This module has the function do pach which generates a .tgz archive
 from the contents of the web_static folder of your AirBnB Clone repo.
 """
 
 from fabric.api import *
 import datetime
 
+
 def do_pack():
+    """
+    This script that generates a .tgz archive
+    from the contents of the web_stati
+    """
     local("mkdir -p versions")
-    filename = "versions/web_static_{}.tgz".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    filename = "versions/web_static_{}.tgz".format(
+            datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+            )
     result = local("tar -cvzf {} web_static".format(filename))
     if result.failed:
         return None
