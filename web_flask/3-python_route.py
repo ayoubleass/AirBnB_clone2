a0c51588ac832267a9aa85fadf243f21dbebeb37
ayoubleass <ayoubleas@gmail.com>
2024-04-19 08:06:27 -0700

    adding task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""This script setup a Flask web application  on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def default():
+    """This functions displays the default content."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ Display HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """ Dispalys <text>"""
+    content = "C " + text.replace('_', ' ')
+    return content
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def py_text(text):
+    """Displays <text>"""
+    content = "Python " + text.replace('_', ' ')
+    return content
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
