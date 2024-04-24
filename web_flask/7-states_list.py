d8fb571cbf7b0b0c984d930e8dbc59cda0cc91c1
ayoubleass <ayoubleas@gmail.com>
2024-04-24 08:34:11 -0700

    fixing task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,18 +3,19 @@
 
 
 from flask import Flask
+from flask import Flask, render_template
+import sys
+sys.path.append('/root/AirBnB_clone_v2/')
 from models import storage
-
-
+import models
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def each_state_cities():
     """display the states and cities"""
-    from models import state
-    states = storage.all(state.State).values()
-    return render_template('8-cities_by_states.html', states=states)
+    states = sorted(storage.all("State"), key= lambda s: s.name)
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
@@ -24,4 +25,4 @@ def teardown_storage(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port='5000', debug=True)
