b1930c8a29b122047a837993ec5fc70785dceaf5
ayoubleass <ayoubleas@gmail.com>
2024-04-24 07:15:48 -0700

    ficing task 7

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ state_2 = State(name="Arizona")
 print("New state: {}".format(state_2))
 state_2.save()
 
+city_1_1 = City(state_id=state_1.id, name="Napa")
+print("New city: {} in the state: {}".format(city_1_1, state_1))
+city_1_1.save()
+city_1_2 = City(state_id=state_1.id, name="Sonoma")
+print("New city: {} in the state: {}".format(city_1_2, state_1))
+city_1_2.save()
+city_2_1 = City(state_id=state_2.id, name="Page")
+print("New city: {} in the state: {}".format(city_2_1, state_2))
+city_2_1.save()
 
 
 """
--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -84,4 +84,4 @@ class DBStorage:
 
     def close(self):
         """ Close the session"""
-        self.__session.remove()
+        self.__session.close()
